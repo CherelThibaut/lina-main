6acab4273e0bbc5f8183f686e4cbb294b46550e9
CFACaronLoic <l.caron@cfa-insta.fr>
2022-02-25 10:45:30 +0100

    front fonctionel

--- front/lina-front/src/app/app.component.ts
+++ front/lina-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Track } from 'ngx-audio-player';   
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lina-front';
+
+  msaapDisplayTitle = true;
+  msaapDisplayPlayList = true;
+  msaapPageSizeOptions = [2,4,6];
+  msaapDisplayVolumeControls = true;
+  msaapDisplayRepeatControls = true;
+  msaapDisplayArtist = false;
+  msaapDisplayDuration = false;
+  msaapDisablePositionSlider = true;
+    
+  // Material Style Advance Audio Player Playlist
+  msaapPlaylist: Track[] = [
+    {
+      title: 'Audio One Title',
+      link: 'https://p.scdn.co/mp3-preview/d69227aeef6ec7f07f446f592f2d1bb5bed2dd12?cid=d36e56f267a34540b2a1d973ac1edc93',
+      artist: 'Audio One Artist',
+      //duration: 'Audio One Duration in seconds'
+    },
+    {
+      title: 'Audio Two Title',
+      link: 'Link to Audio Two URL',
+      artist: 'Audio Two Artist',
+      //duration: 'Audio Two Duration in seconds'
+    },
+    {
+      title: 'Audio Three Title',
+      link: 'Link to Audio Three URL',
+      artist: 'Audio Three Artist',
+      //duration: 'Audio Three Duration in seconds'
+    },
+  ];
+
+  onEnded(event:string) {
+
+  }
 }
